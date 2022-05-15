95b3225209539e3e1e17ff6170538dcd4f8b30b0
brseghese <brseghese@gmail.com>
2022-05-15 20:32:02 -0300

    iniciante tudo eh objeto

---
+++ a_js_iniciante/g_tudo_e_objeto/script.js
@@ -0,0 +1,18 @@
+// nomeie 3 propriedades ou métodos de strings
+let nome = "Bruno ";
+console.log(nome.length);
+console.log(nome.repeat(5));
+console.log(nome.slice(0, 2));
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+let link = document.querySelector(".link");
+console.log(link.addEventListener);
+console.log(link.innerHTML);
+console.log(link.id);
+console.log(link.classList);
+console.log(link.className);
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// clipboard.js
