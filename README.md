1b5ce00d264bd854108a4ad35fbf78989f232b08
brseghese <brseghese@gmail.com>
2022-06-16 17:24:08 -0300

    iniciante arrays e loops

--- README.md
+++ README.md
@@ -6,16 +6,18 @@
 
 - [JavaScript ES6+ Completo](https://www.origamid.com/slide/javascript-completo-es6/#/0101-javascript-completo-es6/1)🔗
 
----
-
 <a href="https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/First_steps/What_is_JavaScript"><img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"></a>
+
+---
 
 <details>
 <summary>Clique para Navegar 🔽</summary>
 
-◽ <a href="#1">JavaScript Para Iniciantes</a> <br>
+- <a href="#1">JavaScript Para Iniciantes</a>
 
 </details>
+
+---
 
 <h3 id="1">📍 JavaScript para Iniciantes</h3>
 
@@ -25,3 +27,4 @@
 - Boolean e Condicionais
 - Funções
 - Objetos
+- Array
--- a_js_iniciante/g_tudo_e_objeto/script.js
+++ a_js_iniciante/g_tudo_e_objeto/script.js
@@ -5,14 +5,14 @@ console.log(nome.repeat(5));
 console.log(nome.slice(0, 2));
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
-let link = document.querySelector(".link");
-console.log(link.addEventListener);
-console.log(link.innerHTML);
-console.log(link.id);
-console.log(link.classList);
-console.log(link.className);
+let btn = document.querySelector(".btn");
+console.log(btn.addEventListener);
+console.log(btn.innerHTML);
+console.log(btn.id);
+console.log(btn.classList);
+console.log(btn.className);
 
 // busque na web um objeto (método) capaz de interagir com o clipboard,
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
 
-// clipboard.js
+// https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
